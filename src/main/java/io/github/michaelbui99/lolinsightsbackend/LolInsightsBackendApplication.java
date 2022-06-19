ad24e59510164afecea206e3a66b7c776a60e2eb
Michael Bui <michaelbui99@hotmail.com>
2022-06-19 21:26:42 +0200

    Initial commit - project created

---
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/LolInsightsBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.michaelbui99.lolinsightsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolInsightsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolInsightsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/michaelbui99/lolinsightsbackend/LolInsightsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.michaelbui99.lolinsightsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolInsightsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
