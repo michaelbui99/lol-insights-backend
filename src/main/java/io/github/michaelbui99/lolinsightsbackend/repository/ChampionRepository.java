c8530ef84a448cdbb9473b46529b5c4010a8e458
Michael Bui <michaelbui99@hotmail.com>
2022-06-19 22:39:37 +0200

    Added ChampionRepository interface

---
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/repository/ChampionRepository.java
@@ -0,0 +1,14 @@
+package io.github.michaelbui99.lolinsightsbackend.repository;
+
+import io.github.michaelbui99.lolinsightsbackend.entity.Champion;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public interface ChampionRepository {
+    List<Champion> getAll();
+    Champion getById(String id) throws NoSuchElementException;
+    Champion update(String id);
+    Champion add(Champion champion);
+    void delete(String id);
+}
