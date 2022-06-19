216d4c4b099aa9b80f8f497c80b83368b67d41e2
Michael Bui <michaelbui99@hotmail.com>
2022-06-19 22:35:32 +0200

    Added champion wrapper entity

---
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/entity/Champion.java
@@ -0,0 +1,77 @@
+package io.github.michaelbui99.lolinsightsbackend.entity;
+
+public class Champion {
+    private com.merakianalytics.orianna.types.core.staticdata.Champion champion;
+    private float winRate;
+    private float pickRate;
+    private float banRate;
+
+    private Champion(ChampionsBuilder builder) {
+        this.winRate = builder.winRate;
+        this.pickRate = builder.pickRate;
+        this.banRate = builder.banRate;
+    }
+
+    public com.merakianalytics.orianna.types.core.staticdata.Champion getChampion() {
+        return champion;
+    }
+
+    public void setChampion(com.merakianalytics.orianna.types.core.staticdata.Champion champion) {
+        this.champion = champion;
+    }
+
+    public float getWinRate() {
+        return winRate;
+    }
+
+    public void setWinRate(float winRate) {
+        this.winRate = winRate;
+    }
+
+    public float getPickRate() {
+        return pickRate;
+    }
+
+    public void setPickRate(float pickRate) {
+        this.pickRate = pickRate;
+    }
+
+    public float getBanRate() {
+        return banRate;
+    }
+
+    public void setBanRate(float banRate) {
+        this.banRate = banRate;
+    }
+
+
+    class ChampionsBuilder {
+        private com.merakianalytics.orianna.types.core.staticdata.Champion champion;
+        private float winRate;
+        private float pickRate;
+        private float banRate;
+
+        public ChampionsBuilder(com.merakianalytics.orianna.types.core.staticdata.Champion champion) {
+            this.champion = champion;
+        }
+
+        public ChampionsBuilder winRate(float winRate) {
+            this.winRate = winRate;
+            return this;
+        }
+
+        public ChampionsBuilder pickRate(float pickRate) {
+            this.pickRate = pickRate;
+            return this;
+        }
+
+        public ChampionsBuilder banRate(float banRate) {
+            this.banRate = banRate;
+            return this;
+        }
+
+        public Champion build() {
+            return new Champion(this);
+        }
+    }
+}
