070293f6827ad30ebc5cb949399e18a96b3b5d0c
Michael Bui <michaelbui99@hotmail.com>
2022-06-27 21:00:21 +0200

    Fixed bug where summoner was not being returned

--- src/main/java/io/github/michaelbui99/lolinsightsbackend/LolInsightsBackendApplication.java
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/LolInsightsBackendApplication.java
@@ -2,15 +2,20 @@ package io.github.michaelbui99.lolinsightsbackend;
 
 import com.merakianalytics.orianna.Orianna;
 import com.merakianalytics.orianna.types.common.Region;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LolInsightsBackendApplication {
     public static void main(String[] args) {
-        Orianna.setRiotAPIKey(System.getenv("RIOT_API_KEY"));
-        Orianna.setDefaultRegion(Region.EUROPE_WEST);
         SpringApplication.run(LolInsightsBackendApplication.class, args);
+        Logger logger = LoggerFactory.getLogger(LolInsightsBackendApplication.class);
+        Orianna.setRiotAPIKey(System.getenv("RIOT_API_KEY"));
+        logger.info("RIOT API Key has been set");
+        Orianna.setDefaultRegion(Region.EUROPE_WEST);
+        logger.info("Default region has been set to EUW");
     }
 
 }
