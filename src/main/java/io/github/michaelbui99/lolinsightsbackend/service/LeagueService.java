5d85aa0157bcaaa756801414163e2cdd3433b413
Michael Bui <michaelbui99@hotmail.com>
2022-07-03 18:36:50 +0200

    javadoc comments for LeagueService interface

--- src/main/java/io/github/michaelbui99/lolinsightsbackend/service/LeagueService.java
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/service/LeagueService.java
@@ -4,5 +4,15 @@ import com.merakianalytics.orianna.types.common.Region;
 import com.merakianalytics.orianna.types.core.league.League;
 
 public interface LeagueService {
+    /**
+     * Fetches Ranked Solo Queue 5v5 league for a summoner
+     *
+     * @param summonerName Name of the summoner
+     * @param region       Region of the summoner
+     * @throws IllegalArgumentException                                                             if <code
+     * >summonerName</code> is empty or null
+     * @throws io.github.michaelbui99.lolinsightsbackend.domain.exception.SummonerNotFoundException if no summoner
+     * with the provided summoner name exists
+     */
     League getLeagueForSummonerByName(String summonerName, Region region);
 }
