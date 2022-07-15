c76e1ccae6b54b3bc62b3881f62fa376aabd8a6e
Michael Bui <michaelbui99@hotmail.com>
2022-07-15 16:56:59 +0200

    Refactor check on valid queue type in SummonersController to seperate method

--- src/main/java/io/github/michaelbui99/lolinsightsbackend/service/LeagueService.java
+++ src/main/java/io/github/michaelbui99/lolinsightsbackend/service/LeagueService.java
@@ -10,10 +10,13 @@ public interface LeagueService {
      *
      * @param summonerName Name of the summoner
      * @param region       Region of the summoner
+     * @param queueType    Queue type of the desired league. If null is provided as queue type, then Ranked Solo
+     *                     Queue will be used as the Queue type
      * @throws IllegalArgumentException                                                             if <code
-     * >summonerName</code> is empty or null
+     *                                                                                              >summonerName</code> is empty or null
      * @throws io.github.michaelbui99.lolinsightsbackend.domain.exception.SummonerNotFoundException if no summoner
-     * with the provided summoner name exists
+     *                                                                                              with the provided
+     *                                                                                              summoner name exists
      */
     League getLeagueForSummonerByName(String summonerName, Region region, QueueType queueType);
 }
